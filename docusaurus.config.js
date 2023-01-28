23c0e53ba7ca63be91fdde43ec95e023b51266c3
shane0 <shane.null@gmail.com>
2023-01-27 18:18:05 -0800

    'config'

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -4,8 +4,8 @@
 const lightCodeTheme = require("prism-react-renderer/themes/github");
 const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 
-const organizationName = "LayZeeDK";
-const projectName = "github-pages-docusaurus";
+const organizationName = "shane0";
+const projectName = "docs";
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
